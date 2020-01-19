4031119c5d006faf4b4c164f08d596743b5beb24
SamuelD <samrenfou@hotmail.com>
2020-01-19 22:26:00 +0100

    Creating partition table options + improvement and clean up

--- myarchinstall.sh
+++ myarchinstall.sh
@@ -1,50 +1,74 @@
 #!/bin/sh
 
+
+abort () {
+    error=$1
+    echo "Error: $1"
+    echo "Aborted"
+    exit
+}
+
 #########
 # FIRST #
 #########
 
-makingGRUBGPTPartitionTable () {
-    swapsize=$(grep MemTotal /proc/meminfo | awk '{print int($2/1000000+0.5)*1.5}' | bc)G
+createPartitionTable () {
+    [[ -n $1 ]] && disk=$1 || abort "Need disk label"
+    num=1
+    echo "Creating partition table"
+    echo "label: gpt\nunit: sectors" > part_table
+    echo "/dev/$disk$num : size= +2M,        type=21686148-6449-6E6F-744E-656564454649" >> part_table && num=$(( num + 1 ))
 
-    # echo Check Boot system
-    # checkefi=$(ls /sys/firmware/efi/efivars/ | wc -l)
+    #swap or not
+    echo "Do you want to create swap partition ? (y/n)"
+    read swapyn
+    until [ "$swapyn" = "y" -o "$swapyn" = "n" ];do
+        echo Please answer y or n
+        read swapyn
+    done
+    if [ $swapyn = "y" ];then
+        swapsize=$(grep MemTotal /proc/meminfo | awk '{print int($2/1000000+0.5)*1.5}' | bc)
+        echo "How many Go do you want ? (default $swapsize Go)"
+        read swapsizebis
+        [[ -n $swapsizebis ]] && swapsize=$swapsizebis
+        swapsize="$swapsize"G
+        partswap="$disk$num"
+        echo "/dev/$disk$num : size= +$swapsize, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F" >> part_table && num=$(( num + 1 ))
+    fi
 
-    # if [ $checkefi = 0 ];then
-    #     boottype="21686148-6449-6E6F-744E-656564454649"
-    # else
-    #     boottype="C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
-    # fi
+    # Root partition
+    partroot="$disk$num"
+    echo "/dev/$partroot : size= +25G,       type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709, attrs=\"LegacyBIOSBootable\"" >> part_table && num=$(( num + 1 ))
 
-    echo "label: gpt
-unit: sectors
+    # Home partition
+    parthome="$disk$num"
+    echo "/dev/$parthome : type=933AC7E1-2EB4-4F13-B844-0E14E2AEF915" >> part_table
 
-/dev/sda1 : size= +2M,        type=21686148-6449-6E6F-744E-656564454649
-/dev/sda2 : size= +$swapsize, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
-/dev/sda3 : size= +25G,       type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709, attrs=\"LegacyBIOSBootable\"
-/dev/sda4 : type=933AC7E1-2EB4-4F13-B844-0E14E2AEF915" > part_table
-    sfdisk /dev/sda < part_table
+    # format disk
+    sfdisk /dev/"$disk" < part_table || abort "sfdisk not completted"
     rm part_table
+}
 
+makefilesystem () {
     echo making filesystem
-    mkfs.ext4 /dev/sda3
-    mkfs.ext4 /dev/sda4
+    mkfs.ext4 /dev/"$partroot"
+    mkfs.ext4 /dev/"$parthome"
 
-    echo making swappartition
-    mkswap /dev/sda2
-    swapon /dev/sda2
+    if [ -n $swapsize ];then
+        echo making swappartition
+        mkswap /dev/"$partswap"
+        swapon /dev/"$partswap"
+    fi
 
-    mount /dev/sda3 /mnt
-
+    mount /dev/"$partroot" /mnt
     mkdir /mnt/home
-
-    mount /dev/sda4 /mnt/home
+    mount /dev/"$parthome" /mnt/home
 }
 
 installArch () {
     echo Installing arch linux and packages
     # TODO: Set up the complete list and sort it (it's a mess!)
-    pacstrap /mnt base base-devel linux linux-firmware i3-gaps git xorg-xinit xorg-server emacs python python-gobject man firefox w3m ncmpcpp mpd mpv youtube-dl mpc alsa-utils pavucontrol dunst libnotify unzip bc openssh xclip imagemagick feh fzf python-pip vim emacs networkmanager grub picom fzf ttf-linux-libertine ttf-inconsolata redshift jq offlineimap davfs2 xdotool zsh arc-gtk-theme xsettingsd
+    pacstrap /mnt base base-devel linux linux-firmware i3-gaps grub vim zsh networkmanager git
 }
 
 generateFSTab () {
@@ -82,7 +106,7 @@ clockandlocale () {
 
 installGrub () {
     echo Installing Grub
-    grub-install --target=i386-pc /dev/sda
+    grub-install --target=i386-pc /dev/"$disk"
     echo creating config file
     sed -i 's/\(GRUB_GFXMODE=\)/\1640x480,/' /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
@@ -105,6 +129,10 @@ createUser () {
     sed -i 's/# \(%wheel ALL=(ALL) ALL\)/\1/' /etc/sudoers
     chsh -s /bin/zsh sam
     passwd sam
+}
+
+installmyshit () {
+    openssh xorg-xinit xorg-server emacs python python-gobject man firefox w3m ncmpcpp mpd mpv youtube-dl mpc alsa-utils pavucontrol dunst libnotify unzip bc xclip imagemagick feh fzf python-pip emacs picom fzf ttf-linux-libertine ttf-inconsolata redshift jq offlineimap davfs2 xdotool arc-gtk-theme xsettingsd
 }
 
 createssh () {
@@ -181,8 +209,9 @@ installNC () {
 case $1 in
     --first) # to be launched first (duh)
         timedatectl set-ntp true
-	[[ -n "$2" ]] && disk="$2" || exit
-        makingGRUBGPTPartitionTable
+	[[ -z "$2" ]] && abort "Need disk label in option (--first disklabel)"
+        createPartitionTable "$2"
+        makefilesystem
         installArch
         generateFSTab
         setupLocalandTimeZone
@@ -201,6 +230,7 @@ case $1 in
         mv /home/myarchinstall.sh /home/sam/
         ;;
     --thirst) # to be launched with the user name
+        installmyshit
         # install from NC auth files
         installNC "authentificationfiles" "${HOME}/.authentification"
         installNC "keepassDBs" "${HOME}/.keepassdb"
